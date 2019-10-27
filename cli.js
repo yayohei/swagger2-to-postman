beecb75b89c2b4e7abbf23fa87865826c6d54d12
yamamoto-yohei <yamamoto.yohei@dena.jp>
2019-10-28 01:01:00 +0900

    add cli script

---
+++ cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+//imort package
+var program = require('commander'),
+    swg2postman = require('swagger2-to-postman'),
+    fs = require('fs'),
+    path = require('path');
+
+convert = (input) => {
+  fs.readFile(input, 'utf8', function (err, text) {
+    var result = new swg2postman().convert(JSON.parse(text));
+    console.log(JSON.stringify(result.collection));
+  });
+}
+
+//main 
+program
+  .option('-i, --input <input_file>', 'swagger.json')
+  .parse(process.argv)
+convert(program.input)
